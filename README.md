25866d8dd1915a46a31ba44fe08e7144651560d6
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-12-15 09:33:26 -0800

    Lesson 7

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # SQL-in-10
-<br />
-Below is a link to the challenge solutions.
-<br />
-https://forta.com/books/0135182794/challenges/
 
-<br/>
-Below is a link to the book's resources/scripts/solutions.
-<br />
-https://forta.com/books/0135182794/
---
+++ lesson7.sql
@@ -0,0 +1,14 @@
+--Lesson 7
+
+--Challenge 1
+SELECT vend_id,
+        vend_name as vname,
+        vend_address AS vaddress,
+        vend_city AS vcity
+FROM Vendors
+ORDER BY vname;
+
+--Challenge 2
+SELECT prod_id, prod_price,
+       prod_price * 0.9 AS sale_price
+FROM Products;
